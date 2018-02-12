468b6633b7752f691fe5ebee84916f6615e0521e
Connor <connor.fowlie@me.com>
2018-02-12 13:09:07 -0500

    Updated Structure

--- lab4/src/lab4/Lab4.java
+++ lab4/src/lab4/Lab4.java
@@ -18,7 +18,7 @@ public class Lab4 {
 	public static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
 	public static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
 
-	//Ultrasonic Sensor
+	// Ultrasonic Sensor
 	private static final Port usPort = LocalEV3.get().getPort("S2");
 
 	// Light Sensor
@@ -37,10 +37,12 @@ public class Lab4 {
 		// Ultrasonic Sensor
 		SensorModes usSensor = new EV3UltrasonicSensor(usPort); // usSensor is the instance
 		SampleProvider usDistance = usSensor.getMode("Distance"); // usDistance provides samples from
-		
+
 		// Localizers
 		final UltrasonicLocalizer ultrasonicLocalizer = new UltrasonicLocalizer();
 		final LightLocalizer lightLocalizer = new LightLocalizer();
+
+		int buttonChoice = Button.waitForAnyPress(); // Record choice (left or right press)
 
 		// clear the display
 		lcd.clear();
@@ -48,66 +50,36 @@ public class Lab4 {
 		// ask the user whether the motors should drive in a square or float
 		lcd.drawString("< Left | Right >", 0, 0);
 		lcd.drawString("       |        ", 0, 1);
-		lcd.drawString(" Ultra | Light  ", 0, 2);
-		lcd.drawString("sonic  |        ", 0, 3);
-		int buttonChoice = Button.waitForAnyPress(); // Record choice (left or right press)
+		lcd.drawString(" fall  | rise   ", 0, 2);
+		lcd.drawString(" edge  | edge   ", 0, 3);
+		buttonChoice = Button.waitForAnyPress(); // Record choice (left or right press)
 
 		if (buttonChoice == Button.ID_LEFT) {
-			// clear the display
-			lcd.clear();
-
-			// ask the user whether the motors should drive in a square or float
-			lcd.drawString("< Left | Right >", 0, 0);
-			lcd.drawString("       |        ", 0, 1);
-			lcd.drawString(" fall  | rise   ", 0, 2);
-			lcd.drawString(" edge  | edge   ", 0, 3);
-			buttonChoice = Button.waitForAnyPress(); // Record choice (left or right press)
-
-			if (buttonChoice == Button.ID_LEFT) {
-				// Display changes in position as wheels are (manually) moved
-				Thread odoThread = new Thread(odometer);
-				odoThread.start();
-				Thread odoDisplayThread = new Thread(odometryDisplay);
-				odoDisplayThread.start();
-				
-				// Falling edge
-				(new Thread() {
-					public void run() {
-						ultrasonicLocalizer.fallingEdge();
-					}
-				}).start();
-			}
-			else if (buttonChoice == Button.ID_RIGHT) {
-				// Display changes in position as wheels are (manually) moved
-				Thread odoThread = new Thread(odometer);
-				odoThread.start();
-				Thread odoDisplayThread = new Thread(odometryDisplay);
-				odoDisplayThread.start();
-				
-				// Rising edge
-				(new Thread() {
-					public void run() {
-						ultrasonicLocalizer.risingEdge();
-					}
-				}).start();
-			}
-			
-			
-
-		} else if (buttonChoice == Button.ID_RIGHT) {
-			(new Thread() {
-				public void run() {
-					// Light Localizer
-//					lightLocalizer.findOrigin(leftMotor, rightMotor, leftRadius, rightRadius, track);
-				}
-			}).start();
-
 			// Display changes in position as wheels are (manually) moved
 			Thread odoThread = new Thread(odometer);
 			odoThread.start();
 			Thread odoDisplayThread = new Thread(odometryDisplay);
 			odoDisplayThread.start();
 
+			// Falling edge
+			(new Thread() {
+				public void run() {
+					ultrasonicLocalizer.fallingEdge();
+				}
+			}).start();
+		} else if (buttonChoice == Button.ID_RIGHT) {
+			// Display changes in position as wheels are (manually) moved
+			Thread odoThread = new Thread(odometer);
+			odoThread.start();
+			Thread odoDisplayThread = new Thread(odometryDisplay);
+			odoDisplayThread.start();
+
+			// Rising edge
+			(new Thread() {
+				public void run() {
+					ultrasonicLocalizer.risingEdge();
+				}
+			}).start();
 		}
 	}
 }
--- lab4/src/lab4/LightLocalizer.java
+++ lab4/src/lab4/LightLocalizer.java
@@ -3,6 +3,7 @@ package lab4;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class LightLocalizer {
+	
 	public static void findOrigin(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, double leftRadius, double rightRadius, double track) {
 	
 	}
