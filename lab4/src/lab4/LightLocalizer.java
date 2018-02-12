26beb96eeef8d6502861f09ec34ff7bd1257e2b7
Connor <connor.fowlie@me.com>
2018-02-12 13:56:20 -0500

    Added ultrasonic package and classes

--- lab4/src/lab4/LightLocalizer.java
+++ lab4/src/lab4/LightLocalizer.java
@@ -4,7 +4,7 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class LightLocalizer {
 	
-	public static void findOrigin(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, double leftRadius, double rightRadius, double track) {
+	public static void findOrigin(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor) {
 	
 	}
 }
---
+++ lab4/src/ultrasonic/UltrasonicController.java
@@ -0,0 +1,9 @@
+package ultrasonic;
+
+
+public interface UltrasonicController {
+
+  public void processUSData(int distance);
+
+  public int readUSDistance();
+}
