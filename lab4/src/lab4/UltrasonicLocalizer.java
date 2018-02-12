b5d7941963964598847485f2c5f1d5deb3613187
Connor <connor.fowlie@me.com>
2018-02-12 13:35:44 -0500

    Added method structure

--- lab4/src/lab4/UltrasonicLocalizer.java
+++ lab4/src/lab4/UltrasonicLocalizer.java
@@ -1,9 +1,37 @@
 package lab4;
 
+import java.util.ArrayList;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+
 public class UltrasonicLocalizer {
 
-	UltrasonicLocalizer(){
+	ArrayList<Integer> distances = new ArrayList<Integer>();
+	
+	double fallingEdge;
+	double risingEdge;
+	double estimate;
+	
+	int d;
+	int k;
+	
+	EV3LargeRegulatedMotor leftMotor;
+	EV3LargeRegulatedMotor rightMotor;
+	EV3UltrasonicSensor ultrasonicSensor; 
+
+	
+	UltrasonicLocalizer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, EV3UltrasonicSensor ultrasonicSensor){
+		// Motors
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
 		
+		// Sensors
+		this.ultrasonicSensor = ultrasonicSensor;
+		
+		// Distance and Noise Margin
+		d = 5;
+		k = 2;
 	}
 	
 	public void fallingEdge() {
@@ -11,7 +39,7 @@ public class UltrasonicLocalizer {
 	}
 	
 	public void risingEdge() {
-		
+		// Begin Spinning
 	}
 	
 }
